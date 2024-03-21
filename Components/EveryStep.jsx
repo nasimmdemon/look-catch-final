b542c1cb99d6ab8e051095dcbf651fdeae050d04
Emon Adi <me@emonadi.com>
2024-03-21 17:40:01 +0600

    Fixed the texts for phone

--- Components/EveryStep.jsx
+++ Components/EveryStep.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 const EveryStep = () => {
   return (
     <section className="max-container padding-x mt-10">
-      <div className="w-full h-[312px] bg-emerald-700 rounded-[50px] md:padding-x py-10 px-5 flex content-center items-center justify-center">
-        <div className="flex justify-between">
-          <div>
+      <div className="w-full h-[312px] bg-emerald-700 rounded-[50px] max-md:h-[512px] max-md:items-center">
+        <div className="flex justify-between p-10 max-md:flex-col-reverse flex-row max-md:items-center">
+          <div className="w-[80%] ">
             <h1 className="text-4xl font-black text-white mb-5 max-lg:text-3xl max-md:text-xl">
               Every step. Ever so green.
             </h1>
-            <p className="max-md:font-sm">
+            <p className="max-md:font-sm text-white">
               In our commitment to sustainability, we're transparent about our
               carbon footprint. Utilize our carbon footprint calculator to track
               your monthly usage and see the environmental impact of your rides.
@@ -17,7 +17,7 @@ const EveryStep = () => {
               footprint and create a greener future.
             </p>
           </div>
-          <div className="relative w-full h-full">
+          <div className="flex w-[20%] h-full justify-center items-">
             <Image
               height={300}
               width={300}
--- Components/Privacy.jsx
+++ Components/Privacy.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const Privacy = () => {
   return (
-    <div id="drivers" className="max-container padding-x">
+    <div id="drivers" className="max-container padding-x mt-24">
       <div className=" w-full h-[500px] max-md:h-[750px]  relative bg-gradient-to-l from-neutral-600 to-neutral-600 rounded-[50px] flex-col md:items-start justify-start items-center inline-flex p-7">
         <p className="text-xl text-white text-bold font-montserrat md:text-4xl">
           Security, Privacy. That’s us.
         </p>
-        <div className="flex items-center justify-center max-md:flex-col mb-10 max-md:mb-0  flex-row mt-3 md:gap-10">
-          <p className="md:text-2xl text-lg text-bold font-montserrat ">
+        <div className="flex items-center justify-center max-md:flex-col mb-10 max-md:mb-0  flex-row mt-3 md:gap-30 ">
+          <p className="md:text-2xl text-lg text-bold font-montserrat text-white ">
             <span className="font-black md:text-4xl">100x</span> Secure Fast
             Adaptable
           </p>
@@ -23,8 +23,10 @@ const Privacy = () => {
               src="/assets/images/icon1.png"
             />
             <div>
-              <p>Threat Detection</p>
-              <p>AI-powered monitoring for lightning-fast defence.</p>
+              <p className="text-white">Threat Detection</p>
+              <p className="text-white">
+                AI-powered monitoring for lightning-fast defence.
+              </p>
             </div>
           </div>
           <div className="flex flex-col justify-center gap-2">
@@ -35,8 +37,10 @@ const Privacy = () => {
               src="/assets/images/icon2.svg"
             />
             <div>
-              <p>Fortified Access Control</p>
-              <p>Multi-layered authentication for ironclad protection</p>
+              <p className="text-white">Fortified Access Control</p>
+              <p className="text-white">
+                Multi-layered authentication for ironclad protection
+              </p>
             </div>
           </div>
         </div>
@@ -49,8 +53,10 @@ const Privacy = () => {
               src="/assets/images/icon1.png"
             />
             <div>
-              <p>Threat Detection</p>
-              <p>AI-powered monitoring for lightning-fast defence.</p>
+              <p className="text-white">Threat Detection</p>
+              <p className="text-white">
+                AI-powered monitoring for lightning-fast defence.
+              </p>
             </div>
           </div>
           <div className="flex flex-col justify-center gap-2">
@@ -61,8 +67,10 @@ const Privacy = () => {
               src="/assets/images/icon2.svg"
             />
             <div>
-              <p>Fortified Access Control</p>
-              <p>Multi-layered authentication for ironclad protection</p>
+              <p className="text-white">Fortified Access Control</p>
+              <p className="text-white">
+                Multi-layered authentication for ironclad protection
+              </p>
             </div>
           </div>
         </div>
